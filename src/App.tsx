1883f09061337b5e3c4afd3c7c672a8254890ff1
Joice(Hyunjin Kim) <96639305+JJineu@users.noreply.github.com>
2023-12-09 22:27:55 +0900

    INIT

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import GlobalStyle from '@styles/GlobalStyle';
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+    </>
+  );
+};
+
+export default App;
---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,6 @@
+import { createGlobalStyle } from 'styled-components';
+import './reset.css';
+
+const GlobalStyle = createGlobalStyle``;
+
+export default GlobalStyle;
