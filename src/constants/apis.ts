2e39fc6e5b5cee2e988c9cfb4461630a1e684fe9
Joice(Hyunjin Kim) <96639305+JJineu@users.noreply.github.com>
2023-12-16 02:40:09 +0900

    Feat: url 추가

--- src/constants/apis.ts
+++ src/constants/apis.ts
@@ -1,10 +1,15 @@
 import { ROUTES } from '.';
 
-const { VITE_APP_BASE_URL, VITE_KAKAO_REST_API_KEY } = import.meta.env;
+const { VITE_APP_BASE_URL, VITE_KAKAO_REST_API_KEY, VITE_SERVER_URL } = import.meta.env;
 
 export const URLS = {
   BASE_URL: VITE_APP_BASE_URL,
+  SERVER_URL: VITE_SERVER_URL,
   KAKAO_CALLBACK_URL: `${VITE_APP_BASE_URL}${ROUTES.CALLBACK_KAKAO}`,
+  OAUTH_KAKAO: '/oauth/authorize',
+  CHAT: '/chat',
+  CHAT_LIST: '/chat/list',
+  CHAT_RECENT: '/chat/message',
 } as const;
 
 export const KAKAO_LOGIN_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${VITE_KAKAO_REST_API_KEY}&redirect_uri=${URLS.KAKAO_CALLBACK_URL}&response_type=code`;
