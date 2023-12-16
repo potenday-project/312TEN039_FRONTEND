0d69e221791d8ab36e2603eb2e29b4bea596c95c
Joice(Hyunjin Kim) <96639305+JJineu@users.noreply.github.com>
2023-12-16 16:38:08 +0900

    Fix: proxy 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  server: {
-    port: 3000,
-    host: '0.0.0.0',
-  },
-  preview: {
-    port: 3000,
-    host: '0.0.0.0',
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react(), tsconfigPaths()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_SERVER_URL,
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+          rewrite: path => path.replace(/^\/api/, ''),
+        },
+      },
+      port: 3000,
+    },
+  };
 });
