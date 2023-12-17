96b4676206b6bfbfe5c955ee897061ee5a211717
Joice(Hyunjin Kim) <96639305+JJineu@users.noreply.github.com>
2023-12-17 10:19:16 +0900

    Remove: 주석 및 console.log 제거

--- src/features/rolling-paper/store.ts
+++ src/features/rolling-paper/store.ts
@@ -11,43 +11,7 @@ interface IRollingPaperStore {
 export const rollingPaperStore = atom<IRollingPaperStore>({
   key: 'rollingPaperStore',
   default: {
-    messages: [
-      {
-        content: '푸바오 사랑해~',
-        memberId: 2,
-        randomName: '임시',
-      },
-      {
-        content: '용인푸씨 한국에서 살게 해주세요 기도 1일차...',
-        memberId: 2,
-        randomName: '임시',
-      },
-      {
-        content: '이건 푸바오 말도 들어봐야 된다',
-        memberId: 3,
-        randomName: '임시',
-      },
-      {
-        content: '푸바오 사랑해 영원한 내 아기 판다 사랑해 푸바오',
-        memberId: 4,
-        randomName: '임시',
-      },
-      {
-        content: '이건 푸바오 말도 들어봐야 된다',
-        memberId: 5,
-        randomName: '임시',
-      },
-      {
-        content: '푸야 건강만 해라',
-        memberId: 6,
-        randomName: '임시',
-      },
-      {
-        content: '알겠다 바오',
-        memberId: 1,
-        randomName: '임시',
-      },
-    ],
+    messages: [],
     loading: false,
     hasNext: false,
   },
--- src/features/rolling-paper/types.ts
+++ src/features/rolling-paper/types.ts
@@ -1,10 +1,3 @@
-// export interface IRMessage {
-//   sender: string;
-//   message: string;
-//   date: string;
-//   nickName: string;
-// }
-
 export interface IListValues {
   rollingPaperId?: number;
   memberId: number;
